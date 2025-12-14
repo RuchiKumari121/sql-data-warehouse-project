3a0dd1d6ff143df648fda0d1924b002bd4313326
RuchiKumari121 <ruchikumari5245@gmail.com>
2025-12-14 15:10:57 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -27,8 +27,8 @@ BEGIN
 	cst_key,
 	TRIM(cst_firstname) AS cst_firstname,
 	TRIM(cst_lastname) AS cst_lastname,
-	CASE WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Married'
-		WHEN  UPPER(TRIM(cst_gndr)) = 'S' THEN 'Single'
+	CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+		WHEN  UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
 		ELSE 'n/a'
 	END cst_marital_status,
 	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
